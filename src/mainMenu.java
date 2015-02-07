5c7a26d6ea913977a23d2c333e234a9202b9d154
cs48timewaster <cs48timewaster@gmail.com>
2015-02-06 21:59:53 -0800

    Updated Main Menu
    Cleaned up the code, encapsulated some functions.

--- src/mainMenu.java
+++ src/mainMenu.java
@@ -1,24 +1,13 @@
-import java.awt.BorderLayout;
+package loginScreen;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 
+import javax.swing.JDialog;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,219 +15,160 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.SwingConstants;
+import javax.swing.Timer;
 
 
-//http://www.java2s.com/Code/Java/Swing-JFC/Createamainmenu.htm
-	
-	
+@SuppressWarnings("serial")
 public class mainMenu extends JPanel implements ActionListener {
-  
-	JPanel panel;
-	protected static ArrayList<Account> allUserAccounts = new ArrayList<Account>();
+	protected boolean disposed=false;
 	
-	mainMenu() {
-    JFrame f = new JFrame("Welcome to the Timewaster");
-    f.setSize(550, 500);
+	private JPanel panel;
+	public JFrame mainJFrame;
+	public JDialog dialog;
+	private JMenu jmFile;
+	private JMenuBar jMenuBar;
+	private GridBagConstraints gbc;
+	
+	
+	protected boolean signedInAsUser=false;
+	protected static boolean signedInAsGuest=true;
+	
+	protected  void setUserSignedInStatus(boolean value){
+		signedInAsUser=value;
+	}
+	protected static void setSignedInAsGuest(boolean value){
+		signedInAsGuest=value;
+	}
+	
+	public void addJMIOpensToJMenu(String name){
+		JMenuItem jmiOpen = new JMenuItem(name);
+		jmiOpen.addActionListener(this);
+		jmFile.add(jmiOpen);
+	}
+	public void addJMenuBarFileTab(){
+		jmFile = new JMenu("File");
+		addJMIOpensToJMenu("Open");
+		addJMIOpensToJMenu("Close");
+		addJMIOpensToJMenu("Save");
+		addJMIOpensToJMenu("Exit");	
+		jMenuBar.add(jmFile);
+	}
+	public void addJMenuBarHelpTab(){
+		JMenu jmHelp = new JMenu("Help");
+		JMenuItem jmiAbout = new JMenuItem("About");
+		jmiAbout.addActionListener(this);
+		jmHelp.add(jmiAbout);
+		jMenuBar.add(jmHelp);
 
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    JMenuBar jmb = new JMenuBar();
-    
-    panel = new JPanel();
-    panel.setLayout(new GridBagLayout());
-    panel.setBackground(Color.GREEN);
-    
-    JLabel creatAccount= new JLabel("Create an Account");
-    JLabel forgotPassword= new JLabel("Forgot PAssword?");
-    JLabel signIn= new JLabel("Sign in");
-    
-    GridBagConstraints c = new GridBagConstraints();
-    c.gridwidth = GridBagConstraints.REMAINDER;
-    
-    c.fill = GridBagConstraints.HORIZONTAL;
-   
-    
-    c.fill = GridBagConstraints.BOTH;
-    c.weightx = 4.0;
-    c.weighty = 4.0;
-    new GridBagLayout();
-    
-     //panel.add(signIn,c);
+	}
+	public void initializeJMenuBar(){
+		jMenuBar = new JMenuBar();
+		addJMenuBarFileTab();
+		addJMenuBarHelpTab();
+	}
+	public void setGridBagConstraints(){
+		gbc = new GridBagConstraints();
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 5.0;
+		gbc.weighty = 5.0;
+	}
+	public void addMainActionButton(String title){
+		JButton signINButton = new JButton(title);
+		panel.add(signINButton, gbc);
+		signINButton.addActionListener(this);
+	}
+	public void addMainScreenButtonsToPanel(){
+		addMainActionButton("Sign In");
+		addMainActionButton("Sign in as Guest");
+		addMainActionButton("Create an account");
+		addMainActionButton("Forgot Password?");
+	}
+	public void addIntroTextToPanel(){
+		JLabel welcome = new JLabel("Welcome to Timewaster!!",SwingConstants.CENTER);
+		JLabel please = new JLabel("Please select an option below",SwingConstants.CENTER);
+		Font ff = new Font("Courier", Font.PLAIN, 32);
+		welcome.setFont(ff);
+		welcome.setForeground(Color.BLUE);
+		panel.add(welcome, gbc);
+		
+		ff = new Font("Serif", Font.PLAIN, 22);
+		please.setFont(ff);
+		please.setForeground(Color.BLUE);
+		panel.add(please, gbc);
+	}
+	public void initializeJPanel(){
+		panel = new JPanel();
+		panel.setBackground(Color.GREEN);
+		panel.setLayout(new GridBagLayout());
+		setGridBagConstraints();
+	}
+	public void initializeViewMainJFrame(){
+		mainJFrame = new JFrame("hurry....");
+		mainJFrame.setSize(550, 500);
+		mainJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainJFrame.setJMenuBar(jMenuBar);
+		mainJFrame.add(panel);
+		mainJFrame.setVisible(true);
+	}
+	Timer timer;
+	public void initializeTimer(){
+		 timer = new Timer(200, new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				if(signedInAsUser) {
+					mainJFrame.setVisible(false);
+					disposed=true;
+					timer.stop();
+				}
+			}
+		});
+		timer.start(); 
+	}
+	
+	
+	public void continueUntilFrameClosed(JFrame checkThisFrame){
+	boolean mainMenuDisposed=false;
+	do{
+		if(checkThisFrame.isVisible()) continue;
+		else {
+			checkThisFrame.dispose();
+			mainMenuDisposed=true;
+		}
+	} while(!mainMenuDisposed);
+}
 
-    JMenu jmFile = new JMenu("File");
-    JMenuItem jmiOpen = new JMenuItem("Open");
-    JMenuItem jmiClose = new JMenuItem("Close");
-    JMenuItem jmiSave = new JMenuItem("Save");
-    JMenuItem jmiExit = new JMenuItem("Exit");
-    jmFile.add(jmiOpen);
-    jmFile.add(jmiClose);
-    jmFile.add(jmiSave);
-    jmFile.addSeparator();
-    jmFile.add(jmiExit);
-    jmb.add(jmFile);
 
-    JMenu jmOptions = new JMenu("Options");
-    JMenu a = new JMenu("A");
-    JMenuItem b = new JMenuItem("B");
-    
-    JMenuItem d = new JMenuItem("D");
-    a.add(b);
-   
-    a.add(d);
-    jmOptions.add(a);
+	public mainMenu() {
+		
+		Account Ludlow = new Account("Christopher","Ludlow","chrisaludlow@gmail.com","cal000lud","password");
+		AccountInventory.allUserAccounts.put("chrisaludlow@gmail.com",Ludlow);
+		
+		
+		initializeJPanel();
+		addIntroTextToPanel();
+		addMainScreenButtonsToPanel();
+		initializeJMenuBar();
+		initializeViewMainJFrame(); //adds the panel and the menubar and sets it to visible
+		dialog = new JDialog(mainJFrame);
+		initializeTimer();
+		continueUntilFrameClosed(mainJFrame);
+		
+	}
 
-    JMenu e = new JMenu("E");
-    e.add(new JMenuItem("F"));
-    e.add(new JMenuItem("G"));
-    jmOptions.add(e);
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		if(refactorTheseGlobalVariables.otherWindowOpen==true) return;
+		String comStr = ae.getActionCommand();
+		if(comStr.equals("Sign In")){
+			new SignInAsUser(this);
+		}
+		else if(comStr.equals("Forgot Password?")){
+			new forgotPassword();
+		}
+		else if(comStr.equals("Create an account")){
+			new createAccount();
+		}
+	}
 
-    jmb.add(jmOptions);
-
-    JMenu jmHelp = new JMenu("Help");
-    JMenuItem jmiAbout = new JMenuItem("About");
-    jmHelp.add(jmiAbout);
-    jmb.add(jmHelp);
-
-    jmiOpen.addActionListener(this);
-    jmiClose.addActionListener(this);
-    jmiSave.addActionListener(this);
-    jmiExit.addActionListener(this);
-    b.addActionListener(this);
-   
-    d.addActionListener(this);
-    jmiAbout.addActionListener(this);
-
-    f.setJMenuBar(jmb);
-    f.setVisible(true);
-    
-    JLabel welcome = new JLabel ("Welcome to Timewaster!!", SwingConstants.CENTER);
-    JLabel please = new JLabel("Please select an option below",SwingConstants.CENTER);
-   
-    
-    Font labelFont = welcome.getFont();
-    String labelText = welcome.getText();
-
-    int stringWidth = welcome.getFontMetrics(labelFont).stringWidth(labelText);
-    int componentWidth = welcome.getWidth();
-    
- // Find out how much the font can grow in width.
-    double widthRatio = (double)componentWidth / (double)stringWidth;
-
-    int newFontSize = (int)(labelFont.getSize() * widthRatio);
-    int componentHeight = welcome.getHeight();
-
-    // Pick a new font size so it will not be larger than the height of label.
-    int fontSizeToUse = Math.min(newFontSize, componentHeight)-1;
-
-    // Set the label's font size to the newly determined size.
-    Font ff= new Font("Courier",Font.PLAIN, 32);
-    welcome.setFont(ff);
-    welcome.setForeground(Color.BLUE);
- //   welcome.setText("Please select an option below");
-    panel.add(welcome,c);
-    
-
-     labelFont = please.getFont();
-     labelText = please.getText();
-
-     stringWidth = please.getFontMetrics(labelFont).stringWidth(labelText);
-     componentWidth = please.getWidth();
-    
- // Find out how much the font can grow in width.
-    
-   
-    // Set the label's font size to the newly determined size.
-     ff= new Font("Serif",Font.PLAIN, 22);
-     
-    please.setFont(ff);
-    please.setForeground(Color.BLUE);
- //   welcome.setText("Please select an option below");
-    panel.add(please,c);
-    
-    JButton signINButton = new JButton("Sign In");
-    panel.add(signINButton, c);
-    signINButton.addActionListener(this);
-    
-    JButton signinGuest= new JButton("Sign In as Guest");
-    panel.add(signinGuest,c);
-    signinGuest.addActionListener(this);
-    
-    
-    
-    JButton createAccount = new JButton("Create an account");
-    panel.add( createAccount,c);
-    createAccount.addActionListener(new ActionListener(){
-    	@Override
-    	public void actionPerformed(ActionEvent e){
-    		 SwingUtilities.invokeLater(new Runnable() {
-
- 				@Override
- 				public void run() {
- 					// TODO Auto-generated method stub
- 					try {
-						new createAccount();
-					} catch (FileNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (ClassNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
- 				}
- 				});
-    	}
-    });
-    
-    JButton forgotPass= new JButton("Forgot Password?");
-    panel.add(forgotPass, c);
-   
-   forgotPass.addActionListener(new ActionListener(){
-    	@Override
-    	public void actionPerformed(ActionEvent ee){
-    		 SwingUtilities.invokeLater(new Runnable() {
-
- 				@Override
- 				public void run() {
- 					// TODO Auto-generated method stub
- 					try {
-						new forgotPassword();
-					} catch (FileNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (ClassNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
- 				}
- 				});
-    	}
-    });
-    
-    
-    f.add( panel);
-    f.setVisible(true);
-  }
-  public void actionPerformed(ActionEvent ae) {
-    String comStr = ae.getActionCommand();
-    System.out.println(comStr + " Selected");
-  }
-  public static void main(String args[]) throws FileNotFoundException, IOException, ClassNotFoundException {
-	  ObjectInputStream fileIn = new ObjectInputStream(new FileInputStream("userinfo.txt"));
-  	allUserAccounts = (ArrayList<Account>) fileIn.readObject();
-  	fileIn.close();
-  	
-  	
-    new mainMenu();
-    
-    
-  	  	    	  FileOutputStream fos = new FileOutputStream("userinfo.txt");
-		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(allUserAccounts);
-		oos.close();
-  }
 }
